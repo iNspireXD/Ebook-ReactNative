ceb6adee7d33b622c2b60591be65311818c5c9c8
Aswin Karki <Aswin.karki44@gmail.com>
2023-07-20 11:15:16 +0545

    added react navigation ffor navigating between screens

--- App.tsx
+++ App.tsx
@@ -1,20 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
+import AppNavigation from "./navigation/appNavigation";
 export default function App() {
-  return (
-    <View className="flex-1 justify-center items-center ">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <AppNavigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+type Props = {};
+
+const LoginScreen = () => {
+  return (
+    <View className="flex-1 justify-center items-center bg-white ">
+      <Text>Hello world !!</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
